2681456e0e8f78bc87e43951cb1bf26d02988f67
charly-perspectives <charly@perspective-s.org>
2024-02-02 14:28:46 +0100

    Create tileset.h

---
+++ src/nxsbuild/tileset.h
@@ -0,0 +1,66 @@
+#ifndef TILESET_H
+#define TILESET_H
+
+#include "deps/json.hpp"
+#include "nexusbuilder.h"
+#include <string>
+#include <vector>
+
+struct BoundingVolume
+{
+    float box[12];
+
+    nlohmann::json toJson() {
+        nlohmann::json j = {
+            {"BoundingVolume",
+                {"box", box}}
+        };
+
+        return j;
+    }
+};
+
+struct Content
+{
+    BoundingVolume boundingVolume;
+    std::string uri;
+    // nlohmann::json toJson() {
+
+    // }
+};
+
+struct Tile
+{
+    float transform[16];
+    BoundingVolume boundingVolume;
+    int geometricError;
+    std::string refine;
+    std::vector<Tile> children ;
+    std::vector<int> childrenIndex;
+};
+
+struct Tileset
+{
+    std::string asset = "1.0";
+    int geometricError = 500;
+    Tile root;
+};
+
+class TilesetBuilder
+{
+public:
+    TilesetBuilder() = delete;
+    TilesetBuilder(const NexusBuilder &nexus) : _nexusStructure(nexus) {};
+    void build();
+private:
+    void makeTile(int tile_index);
+    const NexusBuilder& _nexusStructure;
+    std::vector<Tile> _tiles;
+    Tileset _tileset;
+};
+
+
+
+
+
+#endif // TILESET_H
